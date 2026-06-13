9360e956bfffbcc6d2de6e2d534827e01fc84201
Asif Shiraz <asifshiraz@yahoo.com>
2026-06-12 22:22:17 -0500

    updates

--- src/ui/components/copyright-text.tsx
+++ src/ui/components/copyright-text.tsx
@@ -1,8 +1,5 @@
-"use client";
-
 import { getCopyrightText } from "@/config/brand";
 
-/** Client component for copyright text (needs current year) */
 export function CopyrightText() {
 	return <>{getCopyrightText()}</>;
 }
